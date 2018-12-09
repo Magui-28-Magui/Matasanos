94afff84d61be3aa3a32ebd1631277b66d91e2f7
Magui C <margarita.campos.lagunas@uabc.edu.mx>
2018-12-08 20:48:56 -0800

    template

--- resources/views/index.php
+++ resources/views/index.php
@@ -6,7 +6,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="Health medical template project">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="public/styles/bootstrap4/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="../../public/styles/bootstrap4/bootstrap.min.css">
 <link href="public/plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" type="text/css" href="public/styles/main_styles.css">
 <link rel="stylesheet" type="text/css" href="public/styles/responsive.css">
