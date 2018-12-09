2cb085035717ec4812b0ce9fd8e1e572e9bf3c48
Magui C <margarita.campos.lagunas@uabc.edu.mx>
2018-12-09 10:22:38 -0800

    un commit

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,6 @@ Route::get('/', function () {
     return view('page.home');
 });
 
-
-
 Auth::routes();
 
-/*Route::get('/home', 'HomeController@index')->name('home'); */
+Route::get('/home', 'HomeController@index')->name('home');
