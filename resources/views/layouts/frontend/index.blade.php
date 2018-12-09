3aea53fcddd96d613d2da17b09a917aa4e86477d
Magui C <margarita.campos.lagunas@uabc.edu.mx>
2018-12-08 23:03:17 -0800

    blade

--- resources/views/layouts/frontend/index.blade.php
+++ resources/views/layouts/frontend/index.blade.php
@@ -6,12 +6,16 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
+<!-- <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('css/main_styles.css')}}" rel="stylesheet"/>
+    <link href="{{asset('css/responsive.css')}}" rel="stylesheet"/> -->
 
+    <link rel="stylesheet" type="text/css" href="/public/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/public/assets/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="/public/assets/css/main_styles.css">
+    <link rel="stylesheet" type="text/css" href="/public/assets/css/responsive.css.css">
 
-<link rel="stylesheet" type="text/css" href="/public/assets/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="/public/assets/css/font-awesome.css">
-<link rel="stylesheet" type="text/css" href="/public/assets/css/main_styles.css">
-<link rel="stylesheet" type="text/css" href="/public/assets/css/responsive.css.css">
 
 </head>
 <body>
