25f8a9585425b8ee3e94c7586e2496a8645bf4c1
Magui C <margarita.campos.lagunas@uabc.edu.mx>
2018-12-08 21:53:20 -0800

    templates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,10 +6,10 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="{{asset('styles/bootstrap4/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('plugins/font-awesome-4.7.0/css/font-awesome.min.css')}}" rel="stylesheet">
-    <link href="{{asset('plugins/styles/main_styles.css')}}" rel="stylesheet">
-    <link href="{{asset('plugins/styles/responsive.css')}}" rel="stylesheet">
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/main_styles.css')}}" rel="stylesheet">
+    <link href="{{asset('css/responsive.css')}}" rel="stylesheet">
 
 
 <!--<link rel="stylesheet" type="text/css" href="/public/styles/bootstrap4/bootstrap.min.css">
