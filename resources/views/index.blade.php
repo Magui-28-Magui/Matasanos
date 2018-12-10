417b4a9d71914af6b73a700964777c5c5a13e21f
Magui C <margarita.campos.lagunas@uabc.edu.mx>
2018-12-09 19:58:58 -0800

    un commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -169,7 +169,7 @@
 	</footer>
 </div>
 
-<script src="/public/js/jquery-3.3.1.min.js"></script>
+<script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
 <script src="/public/styles/bootstrap4/popper.js"></script>
 <script src="/public/styles/bootstrap4/bootstrap.min.js"></script>
 <script src="/public/plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
