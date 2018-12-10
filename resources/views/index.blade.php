63644a43dd3715ca14f2930b7b57e5f44ac508a0
Magui C <margarita.campos.lagunas@uabc.edu.mx>
2018-12-09 20:43:24 -0800

    un commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -119,18 +119,7 @@
                             </button>
 
                             <!-- The Modal -->
-                            <div class="modal fade" id="myModal">
-                                <div class="modal-dialog modal-sm">
-                                    <div class="modal-content">
 
-                                        <!-- Modal Header -->
-                                        <div class="modal-header">
-                                            <h4 class="modal-title">Agenda tu cita</h4>
-                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                        </div>
-                                </div>
-                            </div>
-						</div>
 					</div>
 				</div>
 			</div>
