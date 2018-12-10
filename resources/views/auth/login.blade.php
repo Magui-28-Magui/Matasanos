aa48b46232cc862fcb47e090378ec2d87f4198c2
Magui C <margarita.campos.lagunas@uabc.edu.mx>
2018-12-09 22:30:58 -0800

    un commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,148 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('css/main_styles.css')}}" rel="stylesheet"/>
+    <link href="{{asset('css/responsive.css')}}" rel="stylesheet"/>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <header class="header" id="header">
+        <div>
+            <div class="header_top">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <div class="header_top_content d-flex flex-row align-items-center justify-content-start">
+                                <div class="logo">
+                                    <img src="{{asset('images/matasanos.png')}}" style="width:300px">
+                                </div>
+                                <div class="header_top_extra d-flex flex-row align-items-center justify-content-start ml-auto">
+                                    <div class="header_top_nav">
+                                        <ul class="d-flex flex-row align-items-center justify-content-start">
+                                            <li><a href="index.blade.php">Inicio</a></li>
+                                            {{--<li><a href="#">Servicios de emergencias</a></li>--}}
+                                            {{--<li><a href="auth/login.php"> Inicia sesion  </a></li>--}}
+                                        </ul>
+                                    </div>
+                                    {{--<div class="header_top_phone">--}}
+                                    {{--<button class="btn btn-default  dim" type="button">Reserva  tu cita</button>--}}
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                    {{--</div>--}}
+                                </div>
+                                <div class="hamburger ml-auto"><i class="fa fa-bars" aria-hidden="true"></i></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
+    </header>
+    <br> <br> <br>
+
+    <div class="container">
+
+
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Usuarios') }}</div>
+
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo de Usuario') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                                    @endif
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                    @if ($errors->has('password'))
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
-                    </form>
+
+                            <div class="form-group row">
+                                <div class="col-md-6 offset-md-4">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                        <label class="form-check-label" for="remember">
+                                            {{ __('Remember Me') }}
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Iniciar Secion') }}
+                                    </button>
+
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Olvidaste la Contraseña') }}
+                                        </a>
+                                    @endif
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+
+
+    <footer class="footer" style="bottom: 100%">
+        <div class="footer_bar" style="bottom: 100% !important;">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="footer_bar_content d-flex flex-sm-row flex-column align-items-lg-center align-items-start justify-content-start">
+                            <nav class="footer_nav">
+                                <ul class="d-flex flex-lg-row flex-column align-items-lg-center align-items-start justify-content-start">
+                                    <li class="active"><a href="index.blade.php">Inicio</a></li>
+                                    <li><a href="#">Acerca de nosotros</a></li>
+                                    <li><a href="#">Servicios</a></li>
+                                    <li><a href="#">Contacto</a></li>
+                                </ul>
+                            </nav>
+                            <div class="footer_links">
+                                <ul class="d-flex flex-lg-row flex-column align-items-lg-center align-items-start justify-content-start">
+                                    <li><a href="#">Inicio</a></li>
+                                    <li><a href="#">Servicios de emergencias</a></li>
+                                </ul>
+                            </div>
+                            <div class="footer_phone ml-lg-auto">
+                                <i class="fa fa-phone" aria-hidden="true"></i>
+                                <span>+34 586 778 8892</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 @endsection
