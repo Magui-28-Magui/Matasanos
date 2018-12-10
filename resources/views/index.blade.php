ace1d975f1fa202c64af9ea788ffd4610993c572
Magui C <margarita.campos.lagunas@uabc.edu.mx>
2018-12-09 20:03:24 -0800

    un commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -170,11 +170,8 @@
 </div>
 
 <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
-<script src="/public/styles/bootstrap4/popper.js"></script>
-<script src="/public/styles/bootstrap4/bootstrap.min.js"></script>
-<script src="/public/plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
-<script src="/public/plugins/easing/easing.js"></script>
-<script src="/public/plugins/parallax-js-master/parallax.min.js"></script>
-<script src="/public/js/custom.js"></script>
+<script src="plugins/js/custom.js"></script>
+<script src="{{asset('plugins/parallax-js-master/parallax.min.js')}}"></script>
+<script src="{{asset('plugins/js/custom.js')}}"></script>
 </body>
 </html>
